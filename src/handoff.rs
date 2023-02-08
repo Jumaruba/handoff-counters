868d34df211e498b01c4c797d1c78868f0a581c9
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2023-02-08 17:41:29 +0000

    Add create_token and create_slot methods

--- src/handoff.rs
+++ src/handoff.rs
@@ -44,30 +44,7 @@ impl<K> Handoff<K>
         self.vals.insert(self.id.clone(), curr_val);
     }
 
-    /// This function creates a slot for the handoff that will receive information. 
-    fn create_slot(&mut self, h: Handoff<K>) {
-        if self.tier < h.tier && h.val > 0 && !self.slots.contains_key(&h.id) {
-            self.slots.insert(h.id.clone(), (h.sck, self.dck));
-        }
-        self.dck += 1; 
-    }
 
-    /*fn discard_slot(&mut self, h: Handoff<K>) {
-        if (&self.slots.contains_key(&h.id) && h.sck > self.slots.get(&h.id)[0]) {
-            
-        }
-    }*/
-
-    /// Creates a token to send to a node that has requested it by creating a slot. 
-    fn create_token(&mut self, h: Handoff<K>) {
-        let is_waiting_token : bool = h.slots.contains_key(&self.id);
-        let is_slot_valid: bool = h.slots.get(&self.id).unwrap().0.clone() == self.sck; 
-        if  is_waiting_token &&  is_slot_valid {
-            self.tokens.insert((self.id.clone(), h.id.clone()), (self.slots.get(&self.id).unwrap().clone(), self.vals.get(&self.id).unwrap().clone()));
-            self.vals.insert(self.id.clone(), 0);   // Reset the current value of the node. 
-            self.sck += 1;  
-        }
-    }
 
     
     
